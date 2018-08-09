261882b5a874fad7d339efc3c840189662596031
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-09 12:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const distanceFromHqInBlocks = d => Math.abs(42-d);
 const distanceFromHqInFeet = d => distanceFromHqInBlocks(d) * 264;
 const distanceTravelledInFeet = (a,b) => Math.abs(a-b) * 264;
-const calculatesFarePrice = (a,b) => (d => d < 400 ? 0 : d < 2000 ? (d-400) * 0.02 : d < 2500 ? 25 : 'cannot travel that far')(distanceFromHqInFeet(a,b))
+const calculatesFarePrice = (a,b) => (d => d < 400 ? 0 : d < 2000 ? (d-400) * 0.02 : d < 2500 ? 25 : 'cannot travel that far')(distanceTravelledInFeet(a,b))
